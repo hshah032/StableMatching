4ef4f0cce2940d81fbfad6f97849d06b9cf4154d
Malaika Abid <30415707+MalaikaAbid@users.noreply.github.com>
2020-02-16 10:51:50 -0700

    Added method for preferences

--- GaleShapley.java
+++ GaleShapley.java
@@ -11,99 +11,7 @@ import java.util.*;
 	 
 	/** Class GaleShapley **/
 public class GaleShapley {
-	
-	
-	    private int N, engagedCount;
-	    private String[][] menPref;
-	    private String[][] womenPref;
-	    private String[] men;
-	    private String[] women;
-	    private String[] womenPartner;
-	    private boolean[] menEngaged;
-	 
-	    /** Constructor **/
-	    public GaleShapley(String[] m, String[] w, String[][] mp, String[][] wp)
-	    {
-	        N = mp.length;
-	        engagedCount = 0;
-	        men = m;
-	        women = w;
-	        menPref = mp;
-	        womenPref = wp;
-	        menEngaged = new boolean[N];
-	        womenPartner = new String[N];
-	        calcMatches();
-	    }
-	    /** function to calculate all matches **/
-	    private void calcMatches()
-	    {
-	        while (engagedCount < N)
-	        {
-	            int free;
-	            for (free = 0; free < N; free++)
-	                if (!menEngaged[free])
-	                    break;
-	 
-	            for (int i = 0; i < N && !menEngaged[free]; i++)
-	            {
-	                int index = womenIndexOf(menPref[free][i]);
-	                if (womenPartner[index] == null)
-	                {
-	                    womenPartner[index] = men[free];
-	                    menEngaged[free] = true;
-	                    engagedCount++;
-	                }
-	                else
-	                {
-	                    String currentPartner = womenPartner[index];
-	                    if (morePreference(currentPartner, men[free], index))
-	                    {
-	                        womenPartner[index] = men[free];
-	                        menEngaged[free] = true;
-	                        menEngaged[menIndexOf(currentPartner)] = false;
-	                    }
-	                }
-	            }            
-	        }
-	        printCouples();
-	    }
-	    /** function to check if women prefers new partner over old assigned partner **/
-	    private boolean morePreference(String curPartner, String newPartner, int index)
-	    {
-	        for (int i = 0; i < N; i++)
-	        {
-	            if (womenPref[index][i].equals(newPartner))
-	                return true;
-	            if (womenPref[index][i].equals(curPartner))
-	                return false;
-	        }
-	        return false;
-	    }
-	    /** get men index **/
-	    private int menIndexOf(String str)
-	    {
-	        for (int i = 0; i < N; i++)
-	            if (men[i].equals(str))
-	                return i;
-	        return -1;
-	    }
-	    /** get women index **/
-	    private int womenIndexOf(String str)
-	    {
-	        for (int i = 0; i < N; i++)
-	            if (women[i].equals(str))
-	                return i;
-	        return -1;
-	    }
-	    /** print couples **/
-	    public void printCouples()
-	    {
-	        System.out.println("Couples are : ");
-	        for (int i = 0; i < N; i++)
-	        {
-	            System.out.println(womenPartner[i] +" "+ women[i]);
-	        }
-	    }
+
 	    
 	    public static List<String> readColumnOne(String filename) {
 	    	
@@ -196,6 +104,41 @@ public class GaleShapley {
 	    	
 	    }
 	    
+	    public static Map<String, ArrayList<String>> Prefers(List<String> keyArray, String filename){
+	
+	    	 Map<String, ArrayList<String>> Prefers = new HashMap<String, ArrayList<String>>();	
+	    	 
+	    	 int i = 0;
+	    	 while( i < keyArray.size()) {
+		    		
+		    	List<String> valuesFromFile = new ArrayList<String>();
+   		   	    valuesFromFile = readRows(filename, keyArray.get(i));
+			   	Prefers.put(keyArray.get(i), (ArrayList<String>) valuesFromFile);
+		    	
+			   	i++;
+		    	
+		    	}
+	    	
+	    	
+//	    	 Map<String, ArrayList<String>> employeePrefers = new HashMap<String, ArrayList<String>>();	    	
+//		    	
+//		    	int i = 0;
+//		    	while( i < employees.size()) {
+//		    		
+//		    	List<String> studentsFromEFile = new ArrayList<String>();
+//			   	studentsFromEFile = readRows(filename1, employees.get(i));
+//			   	employeePrefers.put(employees.get(i), (ArrayList<String>) studentsFromEFile);
+//
+//		    		
+//		    		i++;
+//		    	
+//		    	}
+	    	 
+	    	 
+	    	 return Prefers;
+	    	
+	    }
+	    
 	    
 	  
 	    /** main function **/
@@ -210,57 +153,12 @@ public class GaleShapley {
 	    	employees = readColumnOne(filename1);
 	    	students = readColumnOne(filename2);
 	    	
-	    	Map<String, ArrayList<String>> employeePrefers = new HashMap<String, ArrayList<String>>();	    	
+	    	Map<String, ArrayList<String>> employeePrefers = new HashMap<String, ArrayList<String>>();	
+	    	Map<String, ArrayList<String>> studentPrefers = new HashMap<String, ArrayList<String>>();	
 	    	
-	    	int i = 0;
-	    	while( i < employees.size()) {
-	    		
-	    		List<String> studentsFromEFile = new ArrayList<String>();
-		    	studentsFromEFile = readRows(filename1, employees.get(i));
-		    	employeePrefers.put(employees.get(i), (ArrayList<String>) studentsFromEFile);
-	    		
-	    	}
-	    	
-//	    	System.out.println(Arrays.asList(employeePrefers));
-	    	System.out.println("hi");
+	    	employeePrefers = Prefers(employees, filename1);
+	    	studentPrefers = Prefers(students, filename2);
 
-	    	
-//	    	static Map<String, ArrayList<String>> employeePrefers = 
-//	    			new HashMap<String, ArrayList<String>>(){{
-//	    		        put(employees.get(0), studentsFromEEfile
-//	    		            Arrays.asList("abi", "eve", "cath", "ivy", "jan", "dee", "fay",
-//	    		            "bea", "hope", "gay"));
-//	    		        put("bob",
-//	    		            Arrays.asList("cath", "hope", "abi", "dee", "eve", "fay", "bea",
-//	    		            "jan", "ivy", "gay"));
-//	    		        put("col",
-//	    		            Arrays.asList("hope", "eve", "abi", "dee", "bea", "fay", "ivy",
-//	    		            "gay", "cath", "jan"));
-//	    		        put("dan",
-//	    		            Arrays.asList("ivy", "fay", "dee", "gay", "hope", "eve", "jan",
-//	    		            "bea", "cath", "abi"));
-//	    		        put("ed",
-//	    		            Arrays.asList("jan", "dee", "bea", "cath", "fay", "eve", "abi",
-//	    		            "ivy", "hope", "gay"));
-//	    		        put("fred",
-//	    		            Arrays.asList("bea", "abi", "dee", "gay", "eve", "ivy", "cath",
-//	    		            "jan", "hope", "fay"));
-//	    		        put("gav",
-//	    		            Arrays.asList("gay", "eve", "ivy", "bea", "cath", "abi", "dee",
-//	    		            "hope", "jan", "fay"));
-//	    		        put("hal",
-//	    		            Arrays.asList("abi", "eve", "hope", "fay", "ivy", "cath", "jan",
-//	    		            "bea", "gay", "dee"));
-//	    		        put("ian",
-//	    		            Arrays.asList("hope", "cath", "dee", "gay", "bea", "abi", "fay",
-//	    		            "ivy", "jan", "eve"));
-//	    		        put("jon",
-//	    		            Arrays.asList("abi", "fay", "jan", "gay", "eve", "bea", "dee",
-//	    		            "cath", "ivy", "hope"));
-//	    		    }};
-
-	        
-	        
 
 
 	    }
